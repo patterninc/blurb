3dc2164cc65b76e1e8c5b0b5bc48a794a2a227a0
Evan McDaniel <eamigo13@gmail.com>
2018-10-17 16:58:18 -0600

    add default metrics

--- lib/blurb/report.rb
+++ lib/blurb/report.rb
@@ -11,9 +11,13 @@ module Blurb
       # required argument checks
       raise ArgumentError.new("params hash must contain a recordType") unless params["recordType"]
 
+      # If no metrics are passed in, use the default metrics
+      metrics = params["metrics"] || get_default_metrics(params["recordType"],params["campaignType"])
+      puts "\nMetrics: #{metrics}"
+
       api_params = {
         "reportDate" => params["reportDate"],
-        "metrics" => params["metrics"]
+        "metrics" => metrics
       }
 
       api_params["segment"] = params["segment"] if params["segment"]
@@ -28,6 +32,71 @@ module Blurb
     def self.download(location, opts = {})
       opts.merge!({:full_path => true, :gzip => true, :no_token => true})
       get_request(location, opts)
+    end
+
+    private
+
+    def self.get_default_metrics(record_type, campaign_type)
+      metrics = [
+        "campaignName",
+        "campaignId",
+        "impressions",
+        "clicks",
+        "cost",
+        "attributedSales14d",
+        "attributedSales14dSameSKU",
+        "attributedConversions14d",
+        "attributedConversions14dSameSKU"
+      ]
+
+      metrics.push(
+        "attributedConversions1d",
+        "attributedConversions7d",
+        "attributedConversions14d",
+        "attributedConversions30d",
+        "attributedConversions1dSameSKU",
+        "attributedConversions7dSameSKU",
+        "attributedConversions30dSameSKU",
+        "attributedUnitsOrdered1d",
+        "attributedUnitsOrdered7d",
+        "attributedUnitsOrdered30d",
+        "attributedSales1d",
+        "attributedSales7d",
+        "attributedSales30d",
+        "attributedSales1dSameSKU",
+        "attributedSales7dSameSKU",
+        "attributedSales30dSameSKU"
+      ) if campaign_type == SPONSORED_PRODUCTS
+
+      metrics.push(
+        "adGroupName",
+        "adGroupId"
+      ) if record_type == AD_GROUPS
+
+      metrics.push(
+        "campaignStatus",
+        "campaignBudget"
+      ) if record_type == CAMPAIGNS
+
+      metrics.push(
+        "campaignBudgetType"
+      ) if record_type == CAMPAIGNS && campaign_type = SPONSORED_BRANDS
+
+      metrics.push(
+        "adGroupName",
+        "adGroupId",
+        "currency",
+        "asin",
+        "sku"
+      ) if record_type == PRODUCT_ADS
+
+      metrics.push(
+        "keywordId",
+        "keywordText",
+        "matchType"
+      ) if record_type == KEYWORDS
+
+      return metrics.join(",")
     end
   end
 end
