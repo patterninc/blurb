fc76c4d4b137ac00d27717385bb922d3e3961b84
Evan McDaniel <eamigo13@gmail.com>
2018-10-22 16:12:07 -0600

    update bid_recommendations to v2

--- lib/blurb/bid_recommendation.rb
+++ lib/blurb/bid_recommendation.rb
@@ -4,21 +4,21 @@ module Blurb
       # required argument checks
       raise ArgumentError.new("params hash must contain an adGroupId") unless params["adGroupId"]
 
-      get_request("/v1/adGroups/#{params["adGroupId"]}/bidRecommendations")
+      get_request("/v2/adGroups/#{params["adGroupId"]}/bidRecommendations")
     end
 
     def self.keyword_recommendations(params = {}, opts = {})
       # required argument checks
       raise ArgumentError.new("params hash must contain an keywordId") unless params["keywordId"]
 
-      get_request("/v1/keywords/#{params["keywordId"]}/bidRecommendations")
+      get_request("/v2/keywords/#{params["keywordId"]}/bidRecommendations")
     end
 
     def self.bulk_keyword_recommendations(params = {}, opts = {})
       # required argument checks
       raise ArgumentError.new("params hash must contain an array of keywordIds") unless params["keywordIds"]
 
-      post_request("/v1/keywords/bidRecommendations", params["keywordIds"])
+      post_request("/v2/keywords/bidRecommendations", params["keywordIds"])
     end
   end
 end
