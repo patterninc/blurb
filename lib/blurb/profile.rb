6c1ec113abe7240e205e13b949491582b143630e
Evan McDaniel <eamigo13@gmail.com>
2018-10-22 16:12:48 -0600

    update profile requests to vw

--- lib/blurb/profile.rb
+++ lib/blurb/profile.rb
@@ -2,11 +2,11 @@ module Blurb
   class Profile < BaseResource
 
     def self.list()
-      profile_request("/v1/profiles")
+      profile_request("/v2/profiles")
     end
 
     def self.retrieve(profile_id)
-      profile_request("/v1/profiles/#{profile_id}")
+      profile_request("/v2/profiles/#{profile_id}")
     end
   end
 end
