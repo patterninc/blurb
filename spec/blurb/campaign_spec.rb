bb56a3a0712485a07ec9c876de51c2a22a3d6e22
Evan McDaniel <eamigo13@gmail.com>
2018-12-13 17:08:14 -0700

    fix test

--- spec/blurb/campaign_spec.rb
+++ spec/blurb/campaign_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Blurb::Campaign do
   describe "campaign crud operations" do
     it "creates and retrieve a campaign" do
       campaign = Blurb::Campaign.create("sp", {
-        "name" => "test",
+        "name" => "test1",
         "campaignType" => "sponsoredProducts",
         "state" => "enabled",
         "dailyBudget" => 10,
