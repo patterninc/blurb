1d8c30608bb78a7bb969112de15c5846db847a80
Evan McDaniel <eamigo13@gmail.com>
2019-08-02 14:41:19 -0600

    update RequestCollectionWithCampaignType

--- lib/blurb/campaign_requests.rb
+++ lib/blurb/campaign_requests.rb
@@ -1,11 +1,6 @@
 require 'blurb/request_collection_with_campaign_type'
 
 class CampaignRequests < RequestCollectionWithCampaignType
-  def initialize(**initialize_params)
-    super(initialize_params)
-    @base_url = "#{@base_url}/v2/#{@campaign_type}/campaigns"
-  end
-
   def create_bulk(create_array)
     create_array = map_campaign_payload(create_array)
     super(create_array)
--- lib/blurb/request_collection_with_campaign_type.rb
+++ lib/blurb/request_collection_with_campaign_type.rb
@@ -2,8 +2,9 @@ require 'blurb/request_collection'
 
 class RequestCollectionWithCampaignType < RequestCollection
 
-  def initialize(campaign_type:, **request_collection_params)
-    super(request_collection_params)
+  def initialize(campaign_type:, resource:, base_url:, headers:)
     @campaign_type = campaign_type
+    @base_url = "#{base_url}/v2/#{@campaign_type}/#{resource}"
+    @headers = headers
   end
 end
