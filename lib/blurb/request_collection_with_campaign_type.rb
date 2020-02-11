781599fe736c013adf4e3913415542f79b34f4e4
Evan McDaniel <eamigo13@gmail.com>
2020-02-11 12:01:46 -0700

    bug fix

--- lib/blurb/request_collection_with_campaign_type.rb
+++ lib/blurb/request_collection_with_campaign_type.rb
@@ -3,7 +3,7 @@ require 'blurb/request_collection'
 class Blurb
   class RequestCollectionWithCampaignType < RequestCollection
 
-    def initialize(campaign_type:, resource:, base_url:, headers:, bulk_api_limit:)
+    def initialize(campaign_type:, resource:, base_url:, headers:, bulk_api_limit: 100)
       @campaign_type = campaign_type
       @base_url = "#{base_url}/v2/#{@campaign_type}/#{resource}"
       @headers = headers
