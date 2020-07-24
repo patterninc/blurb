cefc67aaa50ba7eb06d7dd40eb7358e03a5531da
Shehbaz <shehbazbashir@gmail.com>
2020-07-24 13:41:40 +0100

    Fix url for sd reports

--- spec/blurb/sd_ad_group_requests_spec.rb
+++ spec/blurb/sd_ad_group_requests_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "SD Ad Group Requests" do
     @resource = blurb.active_profile.sd_ad_groups
     @resource_name = 'ad_group'
     @create_hash = {
-      name: Faker::Lorem.word,
+      name: Faker::Lorem.word + rand(100).to_s,
       state: ["enabled", "paused"].sample,
       default_bid: rand(5),
       campaign_id: blurb.active_profile.campaigns(:sd).list(state_filter: 'enabled').first[:campaign_id]
