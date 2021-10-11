e7f403cd1e25589ca9ff5aff28ea2e79a8d9faa0
Sophia-bhs <sophia3lsx@gmail.com>
2021-10-11 16:01:02 +1100

    Check sp

--- lib/blurb/request_collection_with_campaign_type.rb
+++ lib/blurb/request_collection_with_campaign_type.rb
@@ -5,7 +5,7 @@ class Blurb
 
     def initialize(campaign_type:, resource:, base_url:, headers:, bulk_api_limit: 100)
       @campaign_type = campaign_type
-      base_url = campaign_type.to_s == 'sd' or campaign_type.to_s == 'sb' ? base_url : "#{base_url}/v2"
+      base_url = campaign_type.to_s == 'sp' ? "#{base_url}/v2" : base_url
       @base_url = "#{base_url}/#{@campaign_type}/#{resource}"
       @headers = headers
       @api_limit = bulk_api_limit
